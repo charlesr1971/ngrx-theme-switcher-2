0507d8961cb541d11bf9ead032f571a2185eb4be
Charles Robertson <cdesign@btinternet.com>
2023-09-03 21:17:01 +0000

    Update angular.webpack.js and package-lock.json

--- angular.webpack.js
+++ angular.webpack.js
@@ -1,6 +1,7 @@
-module.exports = (config, options) => {
+module.exports = {
   // disable the host check on sandbox
   devServer: {
+    historyApiFallback: true,
     disableHostCheck: true
   }
 };
